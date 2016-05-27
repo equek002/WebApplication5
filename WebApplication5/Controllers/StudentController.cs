062f74eb111e6f9b945c2d0bc60ae90c904d632a
equek002 <equek002@e.ntu.edu.sg>
2016-05-27 16:19:50 +0800

    initial commit

---
+++ WebApplication5/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Student()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Student(StudentModel obj)
+        {
+            ViewBag.Msg = "The student having name " + obj.Name + " of class " + obj.Class + " added =)";
+            return View();
+
+        }
+
+    }
+}
---
+++ WebApplication5/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace WebApplication5.Models
+{
+    public class StudentModel
+    {
+
+        [Required(ErrorMessage = "Student Name is required.")]
+        public string Name { get; set;}
+        [Required(ErrorMessage = "Student Class is required.")]
+        public string Class { get; set; }
+    }
+}
